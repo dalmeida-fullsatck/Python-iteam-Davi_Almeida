52efc7206849e8786cd8b702780b748f1606b256
dalmeida-fullsatck <prfsl.davi@gmail.com>
2026-05-29 14:57:29 -0400

    Q7 da lista - Concluída

--- listas/lista-01/q06.py
+++ listas/lista-01/q06.py
@@ -11,3 +11,31 @@
 # Ao aceitar: 'Senha válida após X tentativa(s).'
 
 # ── Sua solução abaixo ──────────────────────────────────────────────────────
+
+tentativas = 0
+while True:
+      senha = input('\nDigite uma senha, mas tem alguns detalhes\n'
+           '1. Mínimo 8 caracteres.\n'\
+            '2. Pelo menos um dígito \n'\
+            '3. Pelo menos uma letra maiúscula.\n')
+
+      
+      tamanho = len(senha) >= 8 
+            
+      digito = any(caracteres.isdigit() for caracteres in senha)
+
+      maiuscula = any(caracteres.isupper() for caracteres in senha)
+
+      if tamanho and digito and maiuscula: 
+            print(f'\nSenha válida após {tentativas} tentativas!')
+            break
+      if not tamanho: 
+            print('\n- Tente digitar no mínimo 8 caracteres!')
+      if not digito: 
+            print('\n- Tente colocar pelo menos um digito numérico!')
+      if not maiuscula: 
+            print('\n- Tente colocar no mínimo uma letra MAIUSCULA!')
+
+      if not maiuscula or tamanho or digito:
+            tentativas+=1
+      
--- listas/lista-01/q07.py
+++ listas/lista-01/q07.py
@@ -1,6 +1,6 @@
 # Lista 01 — Questão 07: Progressão e Análise
-# Aluno: (seu nome)
-# Data:  (data)
+# Aluno: (Davi Almeida de Oliveira)
+# Data:  (28/05/2026)
 
 # ── Enunciado ───────────────────────────────────────────────────────────────
 # Leia 10 notas (0.0–10.0) com validação (try/except + while para inválidas).
@@ -9,3 +9,48 @@
 # Explique em comentários por que escolheu for ou while em cada parte.
 
 # ── Sua solução abaixo ──────────────────────────────────────────────────────
+
+notas = []
+
+for i in range(0,11):
+
+
+
+    while True: 
+        try:
+            nota = float(input(f'Digite a nota {i}: ')) 
+            if nota < 0.0 or nota > 10.0:
+                raise ValueError('Valor deve ser positivo e menor do que 10.0 ') 
+            notas.append(nota)
+            break
+        except ValueError as e: 
+            print(f'Entrada Inválida! {e}');
+
+maiorNota = max(notas)
+menorNota = min(notas)
+media = sum(notas)/ len(notas)
+
+aprovado =0
+recuperacao = 0
+reprovado = 0
+
+acima_media=0
+for nota in notas:
+    if nota > media:
+        acima_media+=1
+
+for nota in range(0,10):
+    if nota >= 7:
+       aprovado+=1
+    elif nota >=5.0:
+        recuperacao+=1
+    else:
+        reprovado+=1
+print("RELAÇÃO APROVADOS, REPROVADOS E MAIOR E MENOR NOTA\n_____________________________________")
+print(f"Maior nota: {maiorNota:.1f}")
+print(f"Menor nota: {menorNota:.1f}")
+print(f"Média da turma: {media:.1f}")
+print(f"Quantidade acima da média: {acima_media}")
+print(f"Quantidades de aprovados: {aprovado}")
+print(f"Quantidades de reprovados: {reprovado}")
+print(f"Quantidades de recuperação: {recuperacao}")
